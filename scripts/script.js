3d4c2150c9976c46150f67f071f94aa0390d9420
Kayleighhhhh <kayleigh.vdham04@gmail.com>
2025-09-04 16:46:01 +0200

    de match verwijderd en begin aan js

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const candles =
+document.querySelectorAll(".candle");
+
+candles.forEach(candle => {
+    candle.addEventListener("click", () => {
+        candle.classList.toggle("lit");
+    });
+});
